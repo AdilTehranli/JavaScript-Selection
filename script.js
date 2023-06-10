02fbb7a27e0a45a619f660ff6c89fc57c319b746
AdilTehranli <adiljt@code.edu.az>
2023-06-10 19:02:12 +0400

    Created selection

---
+++ script.js
@@ -0,0 +1,38 @@
+let marka = ["Bmw", "Opel", "Ford", "Kia"];
+
+let model = [
+  ["x5", "m4", "m5"],
+  ["astra", "vectra"],
+  ["fusion", "focus"],
+  ["optima", "rio"],
+];
+function check() {
+  let num = document.getElementById("bm");
+  let acc = `<option disabled selected value=''>Marka Select</option>`;
+  for (let i = 0; i < marka.length; i++) {
+    acc += `<option  value="${i}">${marka[i]}</option> `;
+  }
+  
+  num.innerHTML = acc;
+}
+
+check();
+function selected(value) {
+  let val = document.getElementById("op");
+  let acc = `<option disabled selected value=''>Model Select</option>`;
+  for (let j = 0; j < model[value].length; j++) {
+    console.log(model[value][j]);
+    acc += `<option  value="${j}">${model[value][j]}</option> `;
+
+  }
+  
+  val.innerHTML = acc;
+}
+function image(){
+ let image = document.getElementById('im')
+  let img=`<img src="img/2014-BMW-X5-Beauty-Left-Done-Small.jpg" alt="">`
+ console.log(image); 
+
+ 
+ image.innerHTML=img
+}
