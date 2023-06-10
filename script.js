e100166f5976560d3639a28976d7eb073e3685f0
AdilTehranli <adiljt@code.edu.az>
2023-06-11 00:55:04 +0400

    Updated

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let marka = ["Bmw", "Opel", "Ford", "Kia"];
 
 let model = [
-  ["x5", "m4", "m5"],
+  ["m4", "m5"],
   ["astra", "vectra"],
   ["fusion", "focus"],
   ["optima", "rio"],
@@ -12,7 +12,7 @@ function check() {
   for (let i = 0; i < marka.length; i++) {
     acc += `<option  value="${i}">${marka[i]}</option> `;
   }
-  
+
   num.innerHTML = acc;
 }
 
@@ -22,17 +22,20 @@ function selected(value) {
   let acc = `<option disabled selected value=''>Model Select</option>`;
   for (let j = 0; j < model[value].length; j++) {
     console.log(model[value][j]);
-    acc += `<option  value="${j}">${model[value][j]}</option> `;
-
+    acc += `<option onchange="image()" value="${j}">${model[value][j]}</option> `;
   }
-  
+
   val.innerHTML = acc;
 }
-function image(){
- let image = document.getElementById('im')
-  let img=`<img src="img/2014-BMW-X5-Beauty-Left-Done-Small.jpg" alt="">`
- console.log(image); 
+let img_arr = [
+  [`img/m5.jpg`, `img/m4.jpg`],
+  [`img/astra.jpg`, `img/vectra.jpg`],
+  [`img/fusin.jpg`, `img/focus.jpg`],
+  ['img/optim.jpg"', `img/rio.jpg`],
+];
 
- 
- image.innerHTML=img
+function image(value) {
+  let div_image = document.getElementById("img_div");
+  let num = document.getElementById("bm").value;
+  img_div.innerHTML = `<img src='${img_arr[num][value]}'/>`;
 }
